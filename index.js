0a85cf6b62af1048d10c45cf1deb7d765ca50d76
TuanAnhTran2k5 <tranvantuananh01012005@gmail.com>
2025-06-11 17:45:08 +0700

    Display Track

--- index.js
+++ index.js
@@ -1,35 +1,71 @@
-
 let token;
 
-document.addEventListener("DOMContentLoaded", function(){
-    initialApp();
+document.addEventListener("DOMContentLoaded", function () {
+  initialApp();
 });
 
- async function  initialApp(){
-     token = await getSpotifyToken();
-    if(token){
-        getPopularTrack();
-    }
+async function initialApp() {
+  token = await getSpotifyToken();
+  if (token) {
+    const response = await getPopularTrack();
+    displayTrack(response.tracks.items);
+  }
 }
 
-async function getPopularTrack(){
-    try{
-        const response = await axios.get("https://api.spotify.com/v1/search", {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-            params: {
-                q: "Sơn Tùng MTP",
-                type: "track",
-                market: "VN",
-            },
-        });
+async function displayTrack(data) {
+  console.log(data);
+  data.forEach((items) => {
+    console.log(items);
+    // console.log(items.album.images[0].url);
+    const imageURL = items.album.images[0].url;
+    const name = items.name;
+    // tao ra the div
+    const element = document.createElement("div");
+    // gan class cho the div do
+    element.className = "track-card";
 
-        console.log(response);
-    } catch(error){
-        console.log(error);
-    }
+    const artistsName = items.artists.map((artist) => artist.name ).join(", ");
+    // console.log(artistsName);
 
+    //gan noi dung
+    element.innerHTML = `
+      <div class="track-card-container">
+                <img
+                  src="${imageURL}"
+                  alt=""
+                />
+                <h3>${name}</h3>
+                <p>${artistsName}</p>
+              </div> `;
+
+
+
+    // gan nguyen the div do vao track-section
+    const trackSection = document.getElementById("track-section");
+    trackSection.appendChild(element);
+
+
+  });
+}
+
+async function getPopularTrack() {
+  try {
+    const response = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        q: "Sơn Tùng MTP",
+        type: "track",
+        market: "VN",
+      },
+    });
+
+    // console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getSpotifyToken() {
