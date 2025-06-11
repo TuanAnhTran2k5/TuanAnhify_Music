7c04c83f2d5e308972ba76056e8dae271d7a047c
TuanAnhTran2k5 <tranvantuananh01012005@gmail.com>
2025-06-11 20:16:01 +0700

    Play Music

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ async function initialApp() {
 async function displayTrack(data) {
   console.log(data);
   data.forEach((items) => {
-    console.log(items);
+    // console.log(items);
+    console.log(items.id);
+
     // console.log(items.album.images[0].url);
     const imageURL = items.album.images[0].url;
     const name = items.name;
@@ -27,6 +29,10 @@ async function displayTrack(data) {
     const artistsName = items.artists.map((artist) => artist.name ).join(", ");
     // console.log(artistsName);
 
+    element.addEventListener("click", () => {
+      playTrack(items.id, name, artistsName);
+    } );
+
     //gan noi dung
     element.innerHTML = `
       <div class="track-card-container">
@@ -34,8 +40,8 @@ async function displayTrack(data) {
                   src="${imageURL}"
                   alt=""
                 />
-                <h3>${name}</h3>
-                <p>${artistsName}</p>
+                <h3>${truncateText(name, 25)}</h3>
+                <p>${truncateText(artistsName, 15)}</p>
               </div> `;
 
 
@@ -46,6 +52,39 @@ async function displayTrack(data) {
 
 
   });
+}
+
+
+function truncateText(text, number){
+
+  return text.length > number ? text.slice(0, number ) + "..." : text;
+}
+
+function playTrack(id, name, artistsName){
+  // console.log(id);
+  // console.log(name);
+  // console.log(artistsName);
+
+  const iframe =document.getElementById("iframe");
+  iframe.src = `https://open.spotify.com/embed/track/${id}?utm_source=generator&theme=0`
+  
+  const modalName = document.getElementById("modal-name");
+  modalName.innerHTML = name;
+
+  const modal = document.getElementById("modal");
+
+  modal.style.display = "block";
+
+  const modalArtist = document.getElementById("modal-artist");
+  modalArtist.innerHTML = artistsName;
+  
+}
+
+function closeModal(){
+  const modal = document.getElementById("modal");
+   modal.style.display = "none";
+    const iframe =document.getElementById("iframe");
+    iframe.src = " ";
 }
 
 async function getPopularTrack() {
